e301be99f271777d26074a3e956b0d904f820150
fengsuiliuyu <2782532365@qq.com>
2021-02-22 10:55:14 +0800

    小测试

---
+++ Project3/源.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include<string.h>
+int main()
+{
+	string a;
+	cin >> a;
+	if (a.compare("1"))
+	{
+		cout << "1都能打歪来！" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
